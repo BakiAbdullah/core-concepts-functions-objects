66f7b1c8279ff4891081c1d5b57a606d4df3f3b4
BakiAbdullah <shatil605@gmail.com>
2023-01-30 23:50:29 +0600

    Objects and object looping

---
+++ objects.js
@@ -0,0 +1,27 @@
+//? Objects in JavaScript
+
+var student = { 
+  id: 115,
+  name: 'Solaiman Khan',
+  class: 9,
+  marks: 98
+}
+
+var mobile = {
+  brand: 'Samsung',
+  price: 19000,
+  storage: '64gb',
+  camera : '7mp'
+}
+
+var myComputer = {
+  brand: 'hp',
+  price: 27500,
+  color: 'silver',
+  processor: 'i5',
+  ram: '8gb'
+}
+
+myComputer.processor = 'i9'
+
+console.log(myComputer); 
---
+++ properties.js
@@ -0,0 +1,38 @@
+var shoppingCart = {
+  books : 3,
+  sunglass: 1,
+  keyboard: 5,
+  mouse: 1,
+  pen: 25
+}
+
+console.log(shoppingCart.books);
+
+//? When you know the property name, use dot (.) notation
+var penCount = shoppingCart.pen
+
+//? Alternative system
+var penCount2 = shoppingCart["pen"] ;
+
+//? Another system 
+//?Object এর Property or Key গুলোকে Array হিসেবে দিবে।
+var properties = Object.keys(shoppingCart); 
+
+//?Object এর Value গুলোকে Array হিসেবে দিবে।
+var propertyValues = Object.values(shoppingCart);
+// console.log(propertyValues);
+
+//? Dynamic ভাবে property & value পাইতে হলে এভাবে name ,value input nite hbe.
+var propertyName = 'books';
+var propertyValue = shoppingCart[propertyName]
+// console.log(propertyName, propertyValue);
+
+
+//? Set Property Value or name
+shoppingCart.mouse = 15; //1.
+// console.log(shoppingCart);
+//? Another way
+shoppingCart['mouse'] = 29; //2.
+// console.log(shoppingCart);
+shoppingCart[propertyName] = 89; //3.
+// console.log(shoppingCart);
