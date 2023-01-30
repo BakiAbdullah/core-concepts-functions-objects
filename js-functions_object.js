c31296e6d3b987530343bca0de36e84d6adda407
BakiAbdullah <shatil605@gmail.com>
2023-01-30 10:29:57 +0600

    first commit

---
+++ js-functions_object.js
@@ -0,0 +1,11 @@
+// function name(params) {
+  
+// } //~ Function Statement-------------
+
+function startFan() {
+  console.log('Stand up');
+  console.log('Walk towards the switch');
+  console.log('Press the switch');
+}
+
+startFan();
